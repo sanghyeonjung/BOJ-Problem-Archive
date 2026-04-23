94a4bda191455073678b546e058fcdefd8db8970
sanghyeonjung <92612671+sanghyeonjung@users.noreply.github.com>
2026-04-23 12:56:35 +0900

    [Bronze V] Title: 채플이 너무해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34945. 채플이 너무해/채플이 너무해.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+
+int main()
+{
+	cin.tie(0)->sync_with_stdio(0);
+	ll s;
+	cin >> s;
+	if (s <= 5)
+		cout << "Oh My God!";
+	else
+		cout << "Success!";
+}
